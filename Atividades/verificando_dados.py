a9c63600ecfe09b940b26ac36b968b6ab70d8c9b
Luís Kennedy <62557726+KancsSneed@users.noreply.github.com>
2020-08-16 19:46:04 -0300

    Função de login

--- Atividades/verificando_dados.py
+++ Atividades/verificando_dados.py
@@ -1,5 +1,13 @@
+#Variaveis
 user = ['Luís Kennedy']
 password = ['289648137']
 database = [
     user , password
 ]
+
+#Funções
+def login():
+    login_user = input("User:     ")
+    login_password = input("Password:   ")
+#Execução
+login()
