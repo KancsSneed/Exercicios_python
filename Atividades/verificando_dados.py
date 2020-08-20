4d0451f79daf338852340a3e3cc782cc60d50b41
Luís Kennedy <62557726+KancsSneed@users.noreply.github.com>
2020-08-20 18:58:31 -0300

    Terminado

--- Atividades/verificando_dados.py
+++ Atividades/verificando_dados.py
@@ -1,17 +1,23 @@
 #Variaveis
-user = ['Luís Kennedy']
-password = ['289648137']
+user = ['LuisKennedy', 'Lucian123']
+password = ['1234', '4321']
 database = [
     user , password
 ]
 
 #Funções
 def login():
-    login_user = input("User:     ")
+    login_user = input("Username:     ")
     login_password = input("Password:   ")
     if login_user in user and login_password in password:
         print(f"Welcome {login_user}!")
+    else:
+        print("Unfortunately your username or password is wrong")
 
 
 #Execução
+print("""
+    Hello! 
+    Please log writing your username and password!
+""")
 login()
