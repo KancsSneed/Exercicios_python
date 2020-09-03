3eec99988f00e258565a7a456cf774f357763d1f
Luís Kennedy <62557726+KancsSneed@users.noreply.github.com>
2020-09-03 17:11:23 -0300

    feito

---
+++ Atividades/Coisa_rapida.py
@@ -0,0 +1,9 @@
+n = 10
+def contagem_regressiva(n):
+    while n > 0:
+        print(n)
+        n = n -1
+        if n == 0:
+            print("Bi bi bi bi")
+            print("Tá na hora de acordar")
+contagem_regressiva(n)
